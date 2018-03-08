44075a43743dd282dcb64efc6c80505e6b093bb4
huyn <huyn@cvm6h4zv52.cvm.iastate.edu>
2018-03-08 15:01:55 -0600

    update googleCrawler

--- googleCrawler.py
+++ googleCrawler.py
@@ -15,30 +15,50 @@ except ImportError:
         from BeautifulSoup import BeautifulSoup as bs
     except ImportError:
         print ("You need beautifulSoup package buddy")
+
+class Article(object):
+    def __init__self(html):
+        self.html = soup(html).text
+        self.title = None
+        self.author = None
+        self.booktitle = None
+        self.volume = None
+        self.year = None
+        self.title = None
+        self.is_journal = False
+        self.journal = None
+        self.pages   = None
         
 class Parser(object):
     def __init__(self,url):
-        self.url = url+'&scisbd=1'
-        self.driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any'])
+        self.url      = url+'&scisbd=1'
+        self.driver   = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any'])
         self.driver.set_window_size(1124, 850)
         self.driver.get(self.url)
-        self.soup  =  bs(self.driver.page_source)
-        self.url = []
+        self.soup     =  bs(self.driver.page_source)
+        self.urls     = []
         self.articles = [] # list of articles appear on the url, we will retrieve the bibtext file 
-    def retrieve_info(self):
+        self.ages     = []        
+    def retrieve_articles(self):
         elements = self.driver.find_elements_by_class_name('gs_or_cit')
         size     = len(elements)
         for i in range(size):
             elements = self.driver.find_elements_by_class_name('gs_or_cit')
             element  = elements[i]
             element.click()
-            time.sleep(5)
+            time.sleep(1)
             element = self.driver.find_element_by_class_name('gs_citi')
             element.click()
-            time.sleep(5)
+            time.sleep(2)
             self.articles.append(self.driver.page_source)
-            time.sleep(5)
             self.driver.get(self.url)
+    def retrieve_urls(self):
+        tags = self.soup.find_all("h3")
+        for t in tags:
+            href = t.find("a",href= True)
+            self.urls.append(href.attrs['href'])
     def get_articles(self):
         return self.articles
+    def get_urls(self):
+        return self.urls
             
